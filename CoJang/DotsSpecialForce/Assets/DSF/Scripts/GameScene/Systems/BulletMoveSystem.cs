7fd583a349becc380cae97476f372ea13a7991ce
CoJang <bbh1039@naver.com>
2023-05-01 23:34:46 +0900

    Bullet Shoot

--- CoJang/DotsSpecialForce/Assets/DSF/Scripts/GameScene/Systems/BulletMoveSystem.cs
+++ CoJang/DotsSpecialForce/Assets/DSF/Scripts/GameScene/Systems/BulletMoveSystem.cs
@@ -3,43 +3,58 @@ using UnityEngine;
 using Unity.Collections;
 using Unity.Transforms;
 using Unity.Burst;
+using Unity.Mathematics;
 
 [BurstCompile]
 public partial struct BulletMoveSystem : ISystem
 {
-	private EntityQuery bulletQuery;
+    //private EntityQuery bulletQuery;
 
-	public void OnCreate(ref SystemState state)
-	{
-		var bulletQueryBuilder = new EntityQueryBuilder(Allocator.TempJob).WithAll<BulletTag>();
-		bulletQuery = state.GetEntityQuery(bulletQueryBuilder);
-		state.RequireForUpdate(bulletQuery);
+    public void OnCreate(ref SystemState state)
+    {
+        //var bulletQueryBuilder = new EntityQueryBuilder(Allocator.TempJob).WithAll<BulletTag>();
+        //bulletQuery = state.GetEntityQuery(bulletQueryBuilder);
+        //state.RequireForUpdate(bulletQuery);
 
-		bulletQueryBuilder.Dispose();
-	}
+        //bulletQueryBuilder.Dispose();
+    }
 
-	public void OnDestroy(ref SystemState state)
-	{
-	}
+    public void OnDestroy(ref SystemState state)
+    {
+    }
 
-	public void OnUpdate(ref SystemState state)
-	{
-		var deltaTime = SystemAPI.Time.DeltaTime;
+    public void OnUpdate(ref SystemState state)
+    {
+        var deltaTime = SystemAPI.Time.DeltaTime;
 
-		var bulletMovementJob = new BulletMovementJob
-		{
-			DeltaTime = deltaTime,
-		};
-		bulletMovementJob.ScheduleParallel();
-	}
+        var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+
+        var bulletMovementJob = new BulletMovementJob
+        {
+            DeltaTime = deltaTime,
+
+            ecb = ecb,
+        };
+        bulletMovementJob.Schedule();
+    }
 }
 
 public partial struct BulletMovementJob : IJobEntity
 {
-	[ReadOnly] public float DeltaTime;
+    [ReadOnly] public float DeltaTime;
+    public EntityCommandBuffer ecb;
 
-	void Execute(ref LocalTransform transform, in BulletComponent bulletData)
-	{
-		transform.Position += transform.Forward() * (bulletData.BulletSpeed * DeltaTime);
-	}
+    public void Execute(ref LocalTransform transform, in BulletTag bulletData)
+    {
+        transform.Position += transform.Forward() * (bulletData.BulletSpeed * DeltaTime);
+
+        if (math.distancesq(transform.Position, bulletData.spawnedPosition) > bulletData.BulletRange)
+        {
+            if (bulletData.self != Entity.Null)
+            {
+                ecb.DestroyEntity(bulletData.self);
+            }
+        }
+    }
 }
